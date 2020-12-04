6e7d0323f3491590411fe8b7285fcdfe7f2a6057
Hi <suryareddy780@gmail.com>
2020-12-04 19:34:12 +0530

    Images Volley

---
+++ app/src/main/java/edmt/dev/imagevolley/MainActivity.java
@@ -0,0 +1,122 @@
+package edmt.dev.imagevolley;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<MainData> dataArrayList = new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+
+        String url = "https://picsum.photos/v2/list";
+
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+        progressDialog.setCancelable(true);
+        progressDialog.show();
+
+        StringRequest request = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response!=null){
+                    progressDialog.dismiss();
+
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        parseArray(jsonArray);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+
+    }
+
+    private void parseArray(JSONArray jsonArray) {
+        for (int i=0;i<jsonArray.length();i++)
+        {
+            try {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                MainData data = new MainData();
+
+                data.setName(jsonObject.getString("author"));
+                data.setImage(jsonObject.getString("download_url"));
+                dataArrayList.add(data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            listView.setAdapter(new BaseAdapter() {
+                @Override
+                public int getCount() {
+                    return dataArrayList.size();
+                }
+
+                @Override
+                public Object getItem(int position) {
+                    return null;
+                }
+
+                @Override
+                public long getItemId(int position) {
+                    return 0;
+                }
+
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    View view = getLayoutInflater().inflate(R.layout.item_main,null);
+                    MainData data = dataArrayList.get(position);
+
+                    ImageView imageView = view.findViewById(R.id.image_view);
+                    TextView textView = view.findViewById(R.id.text_view);
+
+                    Glide.with(getApplicationContext())
+                            .load(data.getImage())
+                            .diskCacheStrategy(DiskCacheStrategy.ALL)
+                            .into(imageView);
+
+                    textView.setText(data.getName());
+                    return view;
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/edmt/dev/imagevolley/MainData.java
@@ -0,0 +1,21 @@
+package edmt.dev.imagevolley;
+
+public class MainData {
+    private String name,image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
